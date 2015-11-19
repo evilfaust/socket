69f9a8b29a70052eec6d0ae9d4418eec53bbbb7f
evilfaust <oleg.faust@gmail.com>
2015-11-19 23:56:15 +0300

    002

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var io = require('socket.io')(http);
-
-http.createServer(function(req, res) {
+ 
+var app = http.createServer(function(req, res) {
     var urlParsed = url.parse(req.url);
-
+ 
     switch (urlParsed.pathname) {
         case '/':
             sendFile("index.html", res);
             break;
-
+ 
         default:
             res.statusCode = 404;
             res.end("Not found");
     }
-
-    io.on('connection', function(socket){
-        console.log('a user connected');
-        socket.on('disconnect', function(){
-            console.log('user disconnected');
-        });
-    });
-
+ 
 }).listen(3003, function(){
     console.log('server is srart on 3003')
 });
+ 
+var io = require('socket.io')(app);
+    io.on('connection', function(socket){
+        socket.on('chat message', function(msg){
+            io.emit('chat message', msg);
+            console.log('сообщение: ' + msg);
 
-
+        });
+    });
+ 
 function sendFile(fileName, res) {
     var fileStream = fs.createReadStream(fileName);
     fileStream
